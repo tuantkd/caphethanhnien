140f027b8721a9ebf8e8bae822ca3e104635c36f
Tuan Nguyen <nguyenvantuan9a7@gmail.com>
2020-09-14 20:06:56 +0700

    change file layout

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -112,12 +112,8 @@
                     </a>
 
                     <div class="user-menu dropdown-menu">
-                        <a class="nav-link" href="#"><i class="fa fa-user"></i> My Profile</a>
-
-                        <a class="nav-link" href="#"><i class="fa fa-user"></i> Notifications <span class="count">13</span></a>
-
-                        <a class="nav-link" href="#"><i class="fa fa-cog"></i> Settings</a>
-
+                        <a class="nav-link" href="#"><i class="fa fa-user"></i> Hồ sơ cá nhân</a>
+                        <a class="nav-link" href="#"><i class="fa fa-cog"></i> Cài đặt</a>
                         <a class="nav-link" href="#"><i class="fa fa-power-off"></i> Logout</a>
                     </div>
                 </div>
